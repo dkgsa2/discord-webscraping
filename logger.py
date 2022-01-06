63ebc5e73e0a343f76332bc3b3e2d472c03ab243
gilad carmel <dkgsa2@gmail.com>
2022-01-06 21:13:45 +0200

    intial commit

--- logger.py
+++ logger.py
@@ -2,6 +2,11 @@
 import logging
 
 def logger(text):
+    """
+
+    :param text:
+    :return:
+    """
     # Create and configure logger
     logging.basicConfig(filename="autolog.log",
                         format='%(asctime)s %(message)s',
@@ -11,6 +16,6 @@ def logger(text):
     logger = logging.getLogger()
 
     # Setting the threshold of logger to DEBUG
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(loggingE.DEBUG)
     logger.info(text)
 
